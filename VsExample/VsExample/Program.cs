b92d858c0f5def36d5252cba9f5723bec5ed9963
crazywoolf <crazywoolf@mail.ru>
2017-05-04 11:23:10 +0300

    Программа 1. Интерфейс консоли: вывод на экран, работа с цветом

---
+++ VsExample/VsExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string arg in args)
+            {
+
+            }
+
+            ConfigureCUI();
+
+            Console.ReadLine();
+        }
+
+        private static void ConfigureCUI()
+        {
+            //Настройка консольного интерфейса (CUI).
+            Console.Title = "My Rocking App";
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("************************************");
+            Console.WriteLine("**** Welcome to My Rocking App *****");
+            Console.WriteLine("************************************");
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+    }
+}

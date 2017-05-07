038bf9f9fd327fd5007d14164b05515a66e9ba85
crazywoolf <crazywoolf@mail.ru>
2017-05-07 18:00:44 +0300

    Работа с перегруженными методами

---
+++ 04Chapter/Methodoverloading/Methodoverloading/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methodoverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Method Overloading ******\n");
+            Console.WriteLine(Add(10, 10));
+            Console.WriteLine(Add(90000000000000,90000000000000));
+            Console.WriteLine(Add(4.3, 4.4));
+            Console.ReadLine();            
+        }
+        //Перегруженный метов Add()
+        static int Add(int x, int y)
+        { return x + y; }
+        static double Add(double x, double y)
+        { return x + y; }
+        static long Add(long x, long y)
+        { return x + y; }
+    }
+}

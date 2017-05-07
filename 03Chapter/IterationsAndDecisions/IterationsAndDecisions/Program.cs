8e2b22dc910a523149f029c1fe03d3ef1944c396
crazywoolf <crazywoolf@mail.ru>
2017-05-07 15:24:03 +0300

    Итерационные конструкции и конструкции принятия решений.

---
+++ 03Chapter/IterationsAndDecisions/IterationsAndDecisions/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IterationsAndDecisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ForLoopExample();
+            ForEachExamle();
+            LinqQueryOverInts();
+            WhileLoopExample();
+            DoWhileLoopExample();
+            IfElseExample();
+            SwitchExample();
+            SwitchOnStringExample();
+            SwitchOnEnumExample();
+            Console.ReadLine();
+        }
+
+        static void SwitchOnEnumExample()
+        {
+            Console.WriteLine("Enter your favorite day of the week: ");
+            DayOfWeek favDay;
+            try
+            {
+                favDay = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Bad input!");
+                return;
+            }
+            switch (favDay)
+            {
+                case DayOfWeek.Friday:
+                    Console.WriteLine("Yes, Friday rules!");
+                    break;
+                case DayOfWeek.Monday:
+                    Console.WriteLine("Anothe day, anothe dollar");
+                    break;
+                case DayOfWeek.Saturday:
+                    Console.WriteLine("Great day indeed.");
+                    break;
+                case DayOfWeek.Sunday:
+                    Console.WriteLine("Football!!!");
+                    break;
+                case DayOfWeek.Thursday:
+                    Console.WriteLine("Almost Friday...");
+                    break;
+                case DayOfWeek.Tuesday:
+                    Console.WriteLine("At last it is not Monday");
+                    break;
+                case DayOfWeek.Wednesday:
+                    Console.WriteLine("A fine day");
+                    break;
+            }
+        }
+
+        static void SwitchOnStringExample()
+        {
+            Console.WriteLine("C# or VB");
+            Console.WriteLine("Please pick your language preference: ");
+            string langChoice = Console.ReadLine();
+            switch (langChoice)
+            {
+                case "C#":
+                Console.WriteLine("Good choice, C# is a fine language.");
+                break;
+                case "VB":
+                    Console.WriteLine("Whell... good luck with that!");
+                    break;
+            }
+            Console.WriteLine();
+                
+        }
+
+        static void SwitchExample()
+        {
+            Console.WriteLine("1 [C#], 2 [VB]");
+            Console.WriteLine("Please pick your language preference: ");
+            string langChoice = Console.ReadLine();
+            int n = int.Parse(langChoice);
+            switch (n)
+            {
+                case 1:
+                    Console.WriteLine("Good choice, C# is a fine language.");
+                    break;
+                case 2:
+                    Console.WriteLine("Whell... good luck with that!");
+                    break;
+            }
+            Console.WriteLine();
+
+        }
+
+        static void IfElseExample()
+        {
+            string stringData = "My textual data";
+            if(stringData.Length < 0)
+                Console.WriteLine("string is greater than 0 characters");
+        }
+
+        static void DoWhileLoopExample()
+        {
+            string userIsDone = "";
+            do
+            {
+                Console.WriteLine("In do/while loop");
+                Console.WriteLine("Are you done? [yes] [no]: ");
+                userIsDone = Console.ReadLine();
+            } while (userIsDone.ToLower() != "yes");
+            Console.WriteLine();
+        }
+
+        static void WhileLoopExample()
+        {
+            string userIsDone = "";
+            while (userIsDone.ToLower() != "yes")
+            {
+                Console.WriteLine("In while loop");
+                Console.WriteLine("Are you done? [yes] [no]: ");
+                userIsDone = Console.ReadLine();
+            }
+        }
+
+        static void LinqQueryOverInts()
+        {
+            int[] numbers = { 10, 20, 30, 40, 1, 2, 3, 8 };
+            //Запрос LINQ!
+            var subset = from i in numbers where i < 10 select i;
+            Console.WriteLine("Values in subset: ");
+            foreach (var i in subset)
+            {
+                Console.WriteLine("{0} ", i);
+            }
+            Console.WriteLine();
+        }
+
+        static void ForEachExamle()
+        {
+            string[] carTypes = { "Ford", "BMW", "Yugo", "Honda" };
+            foreach (string c in carTypes)
+                Console.WriteLine(c);
+            int[] myInts = { 10, 20, 30, 40 };
+            foreach (int i in myInts)
+                Console.WriteLine(i);
+            Console.WriteLine();
+        }
+
+        static void ForLoopExample()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Number is: {0}", i);
+            }
+        }
+    }
+}

f633c8587556ac58a6f166694b4e331e0e5bc841
crazywoolf <crazywoolf@mail.ru>
2017-05-07 13:15:57 +0300

    Неявно типизированные переменные //для работы с LINQ запросами

---
+++ 03Chapter/implicitlyTypedLocalVars/implicitlyTypedLocalVars/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace implicitlyTypedLocalVars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Fun with Imlicit Typing *****");
+            DeclareExplicitVars();
+            Console.ReadLine();
+        }
+        static void DeclareExplicitVars()
+        {
+            var myInt = 0;
+            var myBool = true;
+            var myString = "Time, marches on...";
+            Console.WriteLine("myInt is a: {0}", myInt.GetType().Name);
+            Console.WriteLine("myBool is a: {0}", myBool.GetType().Name);
+            Console.WriteLine("myString is a: {0}", myString.GetType().Name);
+        }
+    }
+}
